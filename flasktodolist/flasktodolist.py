f72de3da5d4e542dd4fca110eece2b1e8efd120b
xrzse <atmanav04@gmail.com>
2023-12-27 19:06:57 -0500

    Made a checklist feature on the todolist

--- flasktodolist/flasktodolist.py
+++ flasktodolist/flasktodolist.py
@@ -1,9 +1,14 @@
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
+tasks = []
 
-@app.route("/")
+@app.route("/", methods=['GET', 'POST'])
 def todolisthome():
+    if request.method == 'POST':
+        task_input = request.form['task_input']
+        tasks.append(task_input)
+        return render_template('todolisthome.html', tasks=tasks)
     return render_template('todolisthome.html')
 
 if __name__ == '__main__':
