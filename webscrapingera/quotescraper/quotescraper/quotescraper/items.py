2585b5f9a85f1c7ebe3f91894a4b13ed65d66380
xrzse <atmanav04@gmail.com>
2024-01-12 22:33:23 -0500

    more quotescraping

--- webscrapingera/quotescraper/quotescraper/quotescraper/items.py
+++ webscrapingera/quotescraper/quotescraper/quotescraper/items.py
@@ -8,5 +8,9 @@ import scrapy
 
 class QuotescraperItem(scrapy.Item):
     # define the fields for your item here like:
-    # name = scrapy.Field()
+    name = scrapy.Field()
     pass
+
+class QuoteItem(scrapy.Item):
+    quote = scrapy.Field()
+    author = scrapy.Field()
